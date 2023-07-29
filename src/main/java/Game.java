53c70f4124c39adfc80d1ff25c8f4f3d319687be
Mikhail Dukhno <ratercorp@gmail.com>
2023-07-29 17:49:24 +0300

    change Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,46 +1,37 @@
 import java.util.ArrayList;
 
 public class Game {
-    Player player = new Player();
-    ArrayList<String> players = new ArrayList<>();
+
+    ArrayList<Player> players = new ArrayList<>();
+
 
     public void register(Player player) {
-        players.add(player.getName());
+        players.add(player);
     }
-    public int findByName (String name){
+
+    public int findByName(String name) {
+        int index = -1;
         for (int i = 0; i < players.size(); i++) {
-            if (name.contains(players.get(i))) {
-                players.get(i)
+            if (name.equals(players.get(i).getName())) {
+                index = i;
+                break;
             }
+        }
+        return index;
     }
 
-    public void round (String playerName1, String playerName2) {
-
-
-//        for (int i = 0; i < players.size(); i++) {
-//            if (playerName1.contains(players.get(i))) {
-//                playerName1 = players.get(i);
-//                int t = player.getStrength();
-//                break;
-//            }
-//
-//
-//        }
-//        for (int i = 0; i < players.size(); i++) {
-//            if (playerName2.contains(players.get(i))) {
-//                playerName2 = players.get(i);
-//                int b = player.getStrength();
-//                break;
-//            }
-//
-//
-//        }
-//
-//        System.out.println(a.compareTo(b));
-
+    public int round(String playerName1, String playerName2) {
+        int indexPlayerOne = findByName(playerName1);
+        int indexPlayerTwo = findByName(playerName2);
+        Player a = players.get(indexPlayerOne);
+        Player b = players.get(indexPlayerTwo);
+        return a.compareTo(b);
 
     }
 
 }
+
+
+
 
 
